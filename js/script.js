e80705b189a28f2c29bd81b0895532e3375b7256
THIAGO DE LIMA AUGUSTIM <148623853+Thiago-Augustim@users.noreply.github.com>
2026-03-19 20:34:04 -0300

    Implementação tela de fluxo-mesas

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@ var userRoot = {
 
 //Faz a validação do login
 document.getElementById("submit-login").addEventListener("click", function(event) {
+    event.preventDefault(); // Evita o envio do formulário
     var inputNome = document.getElementById("txtNome").value;
     var inputSenha = document.getElementById("password").value;
 
+console.log("TEste: ", inputNome, inputSenha);
+
     if (inputNome === userRoot.nome && inputSenha == userRoot.senha) {
-        alert("Login bem-sucedido!");
+        alert("Login bem-sucedido! Bem-vindo, " + inputNome + "!");
+        window.location.href = "pages/fluxo-mesas.html"; // Redireciona para a página home.html
     } else {
         alert("Nome ou senha incorretos. Tente novamente.");
     }
